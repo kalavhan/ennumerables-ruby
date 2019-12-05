b58282373f37e645171a62a3da2a90347f28f649
kalavhan <47485749+kalavhan@users.noreply.github.com>
2019-12-05 09:47:42 -0700

    Finished my_each_with_index method

--- main.rb
+++ main.rb
@@ -3,8 +3,13 @@ module Enumerable
     return unless array.is_a? Array
     for i in 0...(array.length)
       yield(array[i])
-      puts i
+    end
+  end
+  
+  def my_each_with_index(array)
+    return unless array.is_a? Array
+    for i in 0...(array.length)
+      yield(array[i], i)
     end
   end
 end
-
