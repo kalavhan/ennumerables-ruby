d3c3b68cf8b8865fd458d2f7a06605bb68006713
kalavhan <47485749+kalavhan@users.noreply.github.com>
2019-12-05 12:17:52 -0700

    Finished my_select method

--- main.rb
+++ main.rb
@@ -1,15 +1,25 @@
 module Enumerable
-  def my_each(array)
-    return unless array.is_a? Array
+  def my_each
+    return unless block_given?
+    array = self
     for i in 0...(array.length)
       yield(array[i])
     end
   end
   
-  def my_each_with_index(array)
-    return unless array.is_a? Array
+  def my_each_with_index
+    return unless block_given?
+    array = self
     for i in 0...(array.length)
       yield(array[i], i)
     end
+  end
+
+  def my_select
+    return unless block_given?
+    array = self
+    new_array = []
+    array.my_each(){|x| new_array.push(x) if yield(x)}
+    return new_array
   end
 end
